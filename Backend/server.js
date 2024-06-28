f548666e6d722cf4ce878f9b8e5fdaccfb177b5e
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-06-28 00:10:50 -0400

    Implemented put endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -11,80 +11,125 @@ app.use(express.static("public"))
 app.use(express.json());
 
 const mongoose = require('mongoose');
-const CONNECTION_STRING 
-    =  "mongodb+srv://dbUser:Seneca123@cluster0.utiefr8.mongodb.net/splits?retryWrites=true&w=majority";
+const CONNECTION_STRING
+    = "mongodb+srv://dbUser:Seneca123@cluster0.utiefr8.mongodb.net/splits?retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_STRING)
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "Error connecting to database: "));
 db.once("open", () => {
- console.log("Mongo DB connected successfully.");
+    console.log("Mongo DB connected successfully.");
 });
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-  userName: String,
-  password: String,  
+    userName: String,
+    password: String,
 });
 // model - if the collection does not exist in your databse,mongoose will create it.
 const Users = mongoose.model("users", userSchema);
 
-app.get('/api/users',async (req, res) =>{
-    try{
-        const users = await Users.find().lean().exec();
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await Users.find({}, 'userName').lean().exec();
         res.status(200).send(users)
-    }catch(err){
-        res.send(err)
+    } catch (err) {
+        res.status(500).send(err)
     }
 })
 
-app.get('/api/users/:userName',async (req,res) =>{
+app.get('/api/users/:userName', async (req, res) => {
     const username = req.params.userName
     console.log("User Id: ", username)
-    try{
+    try {
         const result = await Users.findOne({ userName: new RegExp(`^${username}$`, 'i') })
         console.log(result)
-        if(result){
-            res.status(200).send(`User Found: ${result}`)
-        }else{
+        if (result) {
+            res.status(200).send(result)
+        } else {
             res.status(404).send(`User Not Found!`)
         }
-    }catch(err){
+    } catch (err) {
         console.log(`Error Finding User: ${username}`)
         res.status(500).send(`Error Finding User: ${username}`)
     }
 })
 
-app.get('/api/users/password/:userName', async (req, res) =>{
+app.get('/api/users/password/:userName', async (req, res) => {
     username = req.params.userName
-    console.log("User Id: ",username)
-    try{
+    console.log("User Id: ", username)
+    try {
         const result = await Users.findOne({ userName: new RegExp(`^${username}$`, 'i') })
         console.log(result)
-        if(result){
+        if (result) {
             res.status(200).send(result.password)
-        }else{
+        } else {
             res.status(404).send(`User Not Found!`)
         }
-    }catch(err){
+    } catch (err) {
         console.log(`Error Finding User: ${username}`)
         res.status(500).send(`Error Finding User: ${username}`)
     }
 })
 
-app.post('/api/users', (req, res) =>{
-    res.status(200).send(`User added`)
+app.post('/api/users', async (req, res) => {
+    const newUsername = req.body.userName
+    const newPassword = req.body.password
+
+    console.log(`username: ${newUsername}, password: ${newPassword}`)
+
+    try {
+        const result = await Users.findOne({ userName: new RegExp(`^${newUsername}$`, 'i') })
+        if (result) {
+            res.status(400).send(`User already exists!`)
+        } else {
+            const newUser = new Users({
+                userName: newUsername,
+                password: newPassword
+            });
+            console.log(newUser)
+            await newUser.save()
+            console.log(`New User: ${newUser} Added!`)
+            res.status(201).send(`New User: ${newUser} Added!`)
+        }
+    } catch (err) {
+        res.status(500).send(`Error adding new user!`)
+    }
 })
 
-app.put('/api/users', (req, res) =>{
-    res.status(200).send()
+app.put('/api/users',async (req, res) => {
+    const username = req.body.userName
+    const newPassword = req.body.password
+
+    try {
+        const result = await Users.findOne({ userName: new RegExp(`^${username}$`, 'i') })
+        console.log(result)
+        if (result && result.password == newPassword) {
+            res.status(500).send(`New password must be different from the current password`)
+        }
+        else{
+            try{
+                const result = await Users.updateOne({ userName: new RegExp(`^${username}$`, 'i') }, { $set: { password: newPassword } })
+                if (result.modifiedCount > 0){
+                    res.status(200).send(`User: ${username}'s password is updated!`)
+                }else{
+                    res.status(404).send(`User: ${username} not found!`)
+                }
+            }catch(err){
+                res.status(500).send(`Error updating user: ${username}`)
+            }
+        }
+    } catch (err) {
+        console.log(`Error Finding User: ${username}`)
+        res.status(500).send(`Error Finding User: ${username}`)
+    }
 })
 
-app.delete('/api/users', (req, res) =>{
+app.delete('/api/users', (req, res) => {
     res.status(200).send()
 })
 
 const onServerStart = () => {
     console.log("Express http server listening on: " + HTTP_PORT);
     console.log(`http://localhost:${HTTP_PORT}`);
-  };
-  app.listen(HTTP_PORT, onServerStart);
+};
+app.listen(HTTP_PORT, onServerStart);

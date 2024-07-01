2e98b2b9e3d3d94a591ba912fd1df4eacccea4fd
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-07-01 18:36:28 -0400

    Added JSON web tokens to backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
-
-app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
-app.use(express.static("public"))
-
-app.use(express.json());
-
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
+};
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        next(null, {
+            _id: jwt_payload._id,
+            userName: jwt_payload.userName,
+            //password: jwt_payload.password,
+        });
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
+app.use(express.json());
 
 const mongoose = require('mongoose');
 const CONNECTION_STRING
@@ -30,7 +56,7 @@ const userSchema = new Schema({
 // model - if the collection does not exist in your databse,mongoose will create it.
 const Users = mongoose.model("users", userSchema);
 
-app.get('/api/users', async (req, res) => {
+app.get('/api/users',passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
         const users = await Users.find({}, 'userName').lean().exec();
         res.status(200).send(users)
@@ -61,55 +87,63 @@ app.post('/api/users/register', async (req, res) => {
     const inputPassword = req.body.password
     const confirmPassword = req.body.confirmPassword
 
-    if(inputPassword == confirmPassword){
+    if (inputPassword == confirmPassword) {
         bcrypt.hash(inputPassword, 10)
-        .then(async (newPassword) => {
-            console.log(`username: ${newUsername}, password: ${newPassword}`)
+            .then(async (newPassword) => {
+                console.log(`username: ${newUsername}, password: ${newPassword}`)
 
-            try {
-                const result = await Users.findOne({ userName: new RegExp(`^${newUsername}$`, 'i') })
-                if (result) {
-                    res.status(400).send(`User already exists!`)
-                } else {
-                    const newUser = new Users({
-                        userName: newUsername,
-                        password: newPassword
-                    });
-                    console.log(newUser)
-                    await newUser.save()
-                    console.log(`New User: ${newUser} Added!`)
-                    res.status(201).send(`New User: ${newUser} Added!`)
+                try {
+                    const result = await Users.findOne({ userName: new RegExp(`^${newUsername}$`, 'i') })
+                    if (result) {
+                        res.status(400).send(`User already exists!`)
+                    } else {
+                        const newUser = new Users({
+                            userName: newUsername,
+                            password: newPassword
+                        });
+                        console.log(newUser)
+                        await newUser.save()
+                        console.log(`New User: ${newUser} Added!`)
+                        res.status(201).send(`New User: ${newUser} Added!`)
+                    }
+                } catch (err) {
+                    res.status(500).send(`Error: ${err}`)
                 }
-            } catch (err) {
-                res.status(500).send(`Error: ${err}`)
-            }
-        })
-        .catch((err) => {
-            res.status(500).send(err)
-        });
-    }else{
+            })
+            .catch((err) => {
+                res.status(500).send(err)
+            });
+    } else {
         res.status(500).send(`Password does not match`)
     }
 })
 
-app.post('/api/users/login', async (req,res) =>{
+app.post('/api/users/login', async (req, res) => {
     const username = req.body.userName
     const password = req.body.password
+    console.log(`username: ${username}, password: ${password}`)
 
-    try{
+    try {
         const result = await Users.findOne({ userName: new RegExp(`^${username}$`, 'i') })
-        if(result){
-            bcrypt.compare(password, result.password).then((hash) =>{
-                if (hash == true){
-                    res.status(200).send(`Login successful`)
-                }else{
+        if (result) {
+            bcrypt.compare(password, result.password).then((hash) => {
+                if (hash == true) {
+                    let payload = {
+                        _id: result._id,
+                        userName: result.userName,
+                        //password: result.password,
+                    };
+
+                    let token = jwt.sign(payload, jwtOptions.secretOrKey);
+                    res.status(200).send({message: `Login successful`, token: token})
+                } else {
                     res.status(500).send(`Incorrect password for User: ${username}`)
                 }
             })
-        }else{
+        } else {
             res.status(404).send(`User: ${username} not found!`)
         }
-    }catch(err){
+    } catch (err) {
         res.status(404).send(`Error: ${err}`)
     }
 })
@@ -139,7 +173,7 @@ app.put('/api/users/update', async (req, res) => {
                         }
                     }).catch((err) => {
                         res.status(500).send(err)
-                    });            
+                    });
                 }
             });
         }
